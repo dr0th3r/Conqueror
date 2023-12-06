bbd1f3baf75a00e5e434d649a97d23bbdaff3d1f
dr0th3r <dr0th3r.code@gmail.com>
2023-12-06 19:24:51 +0100

    Improve question_modal design

--- src/lib/question_modal.svelte
+++ src/lib/question_modal.svelte
@@ -3,19 +3,21 @@
     export let handleAnswer;
 </script>
 
-<div>
-    <header>
-        <h2>{question.question}</h2>
-    </header>
-    <main>
-        {#each question.answers as answer, id}
-            <button on:click|self={() => handleAnswer(question.correctAnswer === id)}>{answer}</button>
-        {/each}
-    </main>
+<div class="outer">
+    <div class="inner">
+        <header>
+            <h2>{question.question}</h2>
+        </header>
+        <main>
+            {#each question.answers as answer, id}
+                <button on:click|self={() => handleAnswer(question.correctAnswer === id)}>{answer}</button>
+            {/each}
+        </main>
+    </div>
 </div>
 
 <style>
-    div {
+    div.outer {
         position: absolute;
         left: 0;
         top: 0;
@@ -27,8 +29,35 @@
         width: 100%;
     }
 
+    .inner {
+        padding: 10px;
+        background-color: #fff;
+        border-radius: 10px;
+        
+    }
+
     header, main {
-        background-color: red;
-        padding: 20px;
+        padding: 10px;
+        gap: 1rem;
+        width: clamp(200px, 30vw, 800px);
+        display: flex;
+        justify-content: center;
+        flex-direction: column;
+        align-items: center;
+    }
+
+    button {
+        padding: 10px 20px;
+        min-width: 10rem;
+        border: none;
+        background-color: #ddd;
+        border-radius: 10px;
+        cursor: pointer;
+        transition: 1s background-color cubic-bezier(0.075, 0.82, 0.165, 1);
+    }
+
+    button:hover {
+        background-color: blue;
+        color: #fff;
     }
 </style>
